ae486c35687ed2303e68f0198af90cff9af3c09f
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2024-11-19 13:09:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
-class Person {
-  constructor(name, age) {
+function Person(name, age) {
     this.name = name;
     this.age = age;
-  }
+}
 
-  greet() {
+Person.prototype.greet = function() {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-  }
-}
+};
 
-class Employee extends Person {
- 
-  constructor(name, age, jobTitle) {
-    super(name, age); // Call the parent class constructor
+function Employee(name, age, jobTitle) {
+    Person.call(this, name, age);
     this.jobTitle = jobTitle;
-  }
-
-  jobGreet() {
-    console.log(
-      `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
-    );
-  }
 }
+
+Employee.prototype = Object.create(Person.prototype);
+
+Employee.prototype.jobGreet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
+
+Employee.prototype.constructor = Employee;
 
 window.Person = Person;
 window.Employee = Employee;
